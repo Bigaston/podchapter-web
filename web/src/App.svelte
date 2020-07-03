3a3fb7956dc53dab8a57bca6c28725f195e0ad3c
Bigaston <bigaston1@gmail.com>
2020-07-03 16:23:20 +0200

    Importation du test

---
+++ web/src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { Buffer } from "buffer";
+    import ID3 from "./node-id3.js"
+    var toBuffer = require('blob-to-buffer')
+
+    console.log(ID3)
+
+    function salut(e) {
+        toBuffer(e.target.files[0], function (err, buffer) {
+            if (err) throw err
+            
+            console.log(buffer)
+
+            console.log(ID3.read(buffer))
+        })
+    }
+</script>
+
+<style>
+
+</style>
+
+<h1>Hello World</h1>
+
+<input type="file" id="fileinput" accept="mp3" on:change={salut} />
